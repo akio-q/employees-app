71eb733ec6143c6b73c3c82296396129ad3dbd40
Valentyn <wolf.valentyn@gmail.com>
2023-01-28 14:36:03 +0200

    add components' files, add component App

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting of employees at company N</h1>
+            <h2>Total number of employees: </h2>
+            <h2>To receive a premium: </h2>
+        </div>
+    );
+}
+
+export default AppInfo;
